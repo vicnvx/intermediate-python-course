ec4aef3640f9a6adc5a39057636bf30f61c3d6fe
vicnvx <victormlira@outlook.com>
2022-04-24 22:40:33 -0500

    add roll comments

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,12 @@ def main():
   dice_sum = 0
   for i in range(0, dice_rolls):
     roll = random.randint(1, 6)
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll}! Fail! ')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Nice!')
+    else:
+      print(f'You rolled a {roll}')
     dice_sum += roll
   print(f'You have rolled a total of {dice_sum}')
 
